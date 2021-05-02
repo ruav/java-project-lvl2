6a3d73289b811e724d85a2162c62a89055673738
Alexander Rudnev <alexander.rudnev@piano.io>
2021-05-02 17:45:34 +0300

    step 5. Json diff.

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,23 +2,24 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
-import java.io.File;
+import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable{
+public class App implements Callable<Integer> {
 
     @CommandLine.Parameters(index = "0", description = "path to first file")
-    private File filepath1;
+    private String filepath1;
 
-    @CommandLine.Parameters(index = "0", description = "path to second file")
-    private File filepath2;
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String filepath2;
 
     @CommandLine.Option(names = { "-f", "--format=format"}, description = "output format", defaultValue = "stylish")
     private String format = "stylish";
 
     @Override
-    public void run() {
-
+    public Integer call() throws Exception {
+        System.out.println(Differ.generate(filepath1, filepath2, format));
+        return 0;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,57 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class Differ {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        return generate(filepath1, filepath2, "stylish");
+    }
+
+    public static String generate(String filepath1, String filepath2, String format) throws IOException {
+        Map<String, Object> file1 = mapper.readValue(new File(filepath1), Map.class);
+        Map<String, Object> file2 = mapper.readValue(new File(filepath2), Map.class);
+
+        List<String> keys = new ArrayList<>(new HashSet<>() {{
+            addAll(file1.keySet());
+            addAll(file2.keySet());
+        }});
+        keys.sort(String::compareTo);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (String key : keys) {
+            String value1 = file1.containsKey(key) ? file1.get(key).toString() : null;
+            String value2 = file2.containsKey(key) ? file2.get(key).toString() : null;
+            if (value1 != null && value2 != null && value1.equals(value2)) {
+                addValue(sb, " ", key, value1);
+            } else {
+                if (value1 != null) {
+                    addValue(sb, "-", key, value1);
+                }
+                if (value2 != null) {
+                    addValue(sb, "+", key, value2);
+                }
+            }
+        }
+        sb.append("\n}");
+        return sb.toString();
+    }
+
+    private static void addValue(StringBuilder sb, String sign, String key, String value) {
+        sb.append(String.format("\n\t%s %s: %s", sign, key, value));
+//        sb.append("\n\t").append(sign).append(" ").append(key).append(": ").append(value);
+    }
+
+}
